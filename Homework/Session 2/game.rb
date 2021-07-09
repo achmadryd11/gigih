8c042a6ac0a21ebfcd612970dd2e0f896478e6b8
Mikozzan <achmadryd11@.com>
2021-07-09 16:47:14 +0700

    First commit

---
+++ Homework/Session 2/game.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+class Game < Person
+    def initialize(name, hitpoint, attack_damage)
+        super(name, hitpoint, attack_damage)
+        @percentage = 0.8
+    end
+
+    def take_damage(damage)
+        if rand < @percentage
+            puts "#{@name} deflects the attack"
+        else
+            @hitpoint -= damage
+        end
+    end
+end
---
+++ Homework/mongolArcher.rb
@@ -0,0 +1,4 @@
+require_relative 'person'
+
+class MongolArcher < Person
+    
